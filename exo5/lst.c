884a0a342a9031276b910b59f21fa882489d90ce
alesbesdria <melissa@meirsman.fr>
2016-03-26 16:27:07 +0100

    exos5 en cours

--- exo5/lst.c
+++ exo5/lst.c
@@ -6,12 +6,15 @@ struct TElement
 {
 	int nombre;
 	Element *suivant;
+	Element *precedent;
 };
 
 typedef struct TListe Liste;
 struct TListe
 {
 	Element *premier;
+	Element *dernier;
+	Element *courant;
 };
 
 Liste *initialisation()
@@ -27,6 +30,7 @@ Liste *initialisation()
 //	element->suivant = NULL;
 //	liste->premier = element;
 	liste->premier = NULL;
+	liste->dernier = NULL;
 //1//ON NE CREE PAS DE PREMIER ELEMENT.
 	return liste;
 }
@@ -43,11 +47,21 @@ void insertion(Liste *liste, int nvNombre)
 	nouveau->nombre = nvNombre;
 //4//PREMIER EST MAINTENANT DEVENU LE SUIVANT.
 	nouveau->suivant = liste->premier;
+//	nouveau->precedent = liste->dernier;
+//	nouveau->precedent = liste->premier;;
 //5//ON REPLACE LE NOUVEAU EN PREMIER.
+//	liste->premier = nouveau;
+//	liste->dernier = nouveau;
+//	nouveau = liste->dernier;
+//	liste->premier->precedent = liste->dernier;
+//	liste->dernier = nouveau->precedent;
+//	nouveau->precedent = nouveau;
+	nouveau->precedent = liste->dernier;
+	nouveau->precedent = liste->premier;
 	liste->premier = nouveau;
 }
 
-void afficherListe(Liste *liste)
+void afficherListeAvant(Liste *liste)
 {
 //6//ON CREE UN ACTUEL POUR AFFICHER CHAQUE ELEMENTS JUSQU A NULL.
 	if (liste == NULL)
@@ -58,8 +72,27 @@ void afficherListe(Liste *liste)
 
 	while (actuel != NULL)
 	{
-		printf("nombre actuel : %d - adresse : %p - addrliste : %p - addrliste.premier : %p - liste.premier : %p->\n ", actuel->nombre, &actuel->nombre, &liste, &liste->premier, liste->premier);
+//		printf("nombre actuel : %d - adresse : %p - addrliste : %p - addrliste.premier : %p - liste.premier : %p->\n ", actuel->nombre, &actuel->nombre, &liste, &liste->premier, liste->premier);
+		printf("nombre actuel : %d -  ", actuel->nombre);
 		actuel = actuel->suivant;
+	}
+	printf("NULL\n");
+}
+
+void afficherListeArriere(Liste *liste)
+{
+//6//ON CREE UN ACTUEL POUR AFFICHER CHAQUE ELEMENTS JUSQU A NULL.
+	if (liste == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	Element *courant = liste->dernier;
+
+	while (courant != NULL)
+	{
+//		printf("nombre courant : %d - adresse : %p - addrliste : %p - addrliste.dernier : %p - liste.dernier : %p->\n ", courant->nombre, &courant->nombre, &liste, &liste->dernier, liste->dernier);
+		printf("nombre courant : %d -  ", courant->nombre);
+		courant = courant->precedent;
 	}
 	printf("NULL\n");
 }
@@ -76,8 +109,17 @@ void suppression(Liste *liste)
 		Element *aSupprimer = liste->premier;
 		liste->premier = liste->premier->suivant;
 		free(aSupprimer);
-		afficherListe(liste);
+//		afficherListe(liste);
 	}
+/*	while (liste->dernier != 0)
+	{
+//7'//ON BOUCLE POUR SUPPRIMER CHAQUE ELEMENT JUSQUAU PREMIER.
+		Element *aSupprimer = liste->dernier;
+		liste->dernier = liste->dernier->precedent;
+		free(aSupprimer);
+//		afficherListe(liste);
+	}
+*/
 //***LIBERATION DE LISTE***//
 	free(liste);
 }
@@ -94,7 +136,8 @@ int main()
 	insertion(maListe, 18);
 //	suppression(maListe);
 
-	afficherListe(maListe);
+	afficherListeAvant(maListe);
+	afficherListeArriere(maListe);
 
 	return 0;
 }
