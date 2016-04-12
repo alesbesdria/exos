59196ee2e7f2421f2c7c36e15addbc558d4612a1
Melissa MEIRSMAN <mmeirsma@e1r4p2.42.fr>
2016-04-12 15:50:41 +0200

    norminette jpm.c

---
+++ jpm.c
@@ -0,0 +1,137 @@
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/other/ft_isascii.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/other/ft_isspace.c
+Error (line 15): Bad spacing for argument s
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/other/ft_isalnum.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/other/ft_swap.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/other/ft_atoi_base.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/includes/libft.h
+Error: file must end with a single empty line
+Error: 42 header not at top of the file
+Error (line 120, col 27): space after pointer
+Error (line 126): line has 89 characters
+Error (line 130): must have tabulations between TList and List
+Error (line 130): must have only one space between struct and List.
+Error (line 130, col 7): bad spacing
+Error (line 130, col 11): TList not unixcase
+Error (line 130, col 24): List not unixcase
+Error (line 130, col 24): List have not a good prefix.
+Error (line 131): must have only tabulations between struct and TList.
+Error (line 131, col 0): TList not unixcase
+Error (line 131, col 0): TList is not well prefixed.
+Error (line 131, col 6): bad spacing
+Error (line 137): line has 83 characters
+Error (line 137, col 20): Source not unixcase
+Error (line 137, col 26): space after pointer
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/list/ft_lstadd.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/list/ft_lstiter.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/list/ft_lstdelone.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/list/ft_lstmap.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/printf/pf_itoa.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/list/ft_lstnew.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/list/ft_lstdel.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/list/ft_lstshift.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/list/ft_lstend.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/list/ft_lstinser.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/list/ft_lstlen.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_putstr_fd.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_putnbr.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_boardprint.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_putnbr_fd.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/printf/pf_solve_flags.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_putstr.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_putnstr.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_putchar.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_putchar_fd.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_putnstr_fd.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_isprint.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_putendl_fd.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_putendl.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/integer/ft_isdigit.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/integer/ft_pwr.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/integer/ft_itoa.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/integer/ft_isnumber.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/printf/pf_apply_flags.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_toupper.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/integer/ft_intboardset.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_gnl.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_striteri.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_wstrtostr.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strcpy.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strstr.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strncat.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strncmp.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strchr.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_wtoc.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_isalpha.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strtoupper.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strdel.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strtrim.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strnew.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strequ.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strnstr.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strsplit.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_atoi.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strsub.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strrchr.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strmapi.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strlen.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strjoin.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_striter.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strclr.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/display/ft_printf.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strmap.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strnequ.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strncpy.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strcmp.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strcat.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_tolower.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/realloclili/jpm.c
+Warning: /nfs/2015/m/mmeirsma/test/libft/src/realloclili/jpm.c may not compile or is invalid for some reasons.
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strlcat.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_strdup.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/memory/ft_memmove.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/string/ft_boardset.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/memory/ft_memccpy.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/memory/ft_memchr.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/memory/ft_memalloc.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/memory/ft_memset.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/memory/ft_memdel.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/memory/ft_memcpy.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/memory/ft_bzero.c
+Norme: /nfs/2015/m/mmeirsma/test/libft/src/memory/ft_memcmp.c
+Norme: /nfs/2015/m/mmeirsma/test/get_next_line.h
+Error: 42 header not at top of the file
+Error: global scope bad aligned
+Error (line 4): wrong number of spaces in preprocessor indentation
+Error (line 5): wrong number of spaces in preprocessor indentation
+Error (line 6): wrong number of spaces in preprocessor indentation
+Error (line 10): multiple empty lines
+Error (line 11): must have tabulations between TElement and Elem
+Error (line 11, col 8): TElement not unixcase
+Error (line 11, col 24): Elem not unixcase
+Error (line 11, col 24): Elem have not a good prefix.
+Error (line 12): must have only tabulations between struct and TElement.
+Error (line 12): Struct TElement bad aligned
+Error (line 12, col 0): TElement not unixcase
+Error (line 12, col 0): TElement is not well prefixed.
+Error (line 15, col 11): FilePosition not unixcase
+Error (line 16, col 11): FileNbCharRead not unixcase
+Error (line 20): comment not well formatted
+Error (line 49): C++ comment
+Error (line 50, col 38): space after pointer
+Norme: /nfs/2015/m/mmeirsma/test/get_next_line.c
+Error: 42 header not at top of the file
+Error (line 3, col 35): space after pointer
+Error (line 3, col 43): Curr not unixcase
+Error (line 3, col 48): space after pointer
+Error (line 3, col 56): nbCharLine not unixcase
+Error (line 32, col 40): space after pointer
+Error (line 34): bad indentation
+Error (line 34, col 11): Curr not unixcase
+Error (line 35): bad indentation
+Error (line 35, col 17): myList not unixcase
+Error (line 36): bad indentation
+Error (line 39, col 8): missing space around <
+Error (line 43, col 13): space after arrow
+Error (line 55, col 32): bad character after ,
+
